7a62492ec4b6d56f5c4cba22fa389913d87f8bea
student <student@ip-172-31-54-163.us-west-2.compute.internal>
2021-02-22 19:59:46 -0800

    I started test scores

---
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -0,0 +1,8 @@
+package _01_double._2_test_scores;
+import javax.swing.JOptionPane;
+
+public class TestScores {
+public static void main(String[] args) {
+	String test=JOptionPane.showInputDialog("what is your test score?");
+}
+}

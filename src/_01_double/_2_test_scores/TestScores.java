f0e8d28cf4c72e491cb5eb13db901baec2868e40
root <root@357c477cc5d9>
2021-02-28 20:53:47 +0000

    i coded

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -4,5 +4,7 @@ import javax.swing.JOptionPane;
 public class TestScores {
 public static void main(String[] args) {
 	String test=JOptionPane.showInputDialog("what is your test score?");
+	double score=Double.parseDouble(test);
+	
 }
 }

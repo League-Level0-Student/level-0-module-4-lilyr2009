5ea768d9bf013dc138fb8c26c29b432897514fe7
student <student@ip-172-31-56-115.us-west-2.compute.internal>
2021-03-28 12:57:33 -0700

    i started test scores

--- src/_01_double/_2_test_scores/TestScores.java
+++ src/_01_double/_2_test_scores/TestScores.java
@@ -6,6 +6,17 @@ public static void main(String[] args) {
 	String test=JOptionPane.showInputDialog("what is your test score?");
 	double score=Double.parseDouble(test);
 	
+	if(score==100) {
+		JOptionPane.showMessageDialog(null, "great job!");
+	}
+	
+	else if(score>90) {
+		JOptionPane.showMessageDialog(null, "good job");
+		
+	}
+	else if(score<90) {
+		JOptionPane.showMessageDialog(null, "eh");
+	}
 	
 }
 }
